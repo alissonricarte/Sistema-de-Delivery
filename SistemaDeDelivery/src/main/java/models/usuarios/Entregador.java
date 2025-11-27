268812c85c67aa75348c4a4461aed44275d8d62a
mateussspinheiro <pinheiromateus900@gmail.com>
2025-11-27 15:03:29 -0300

    feat: melhorias na classe entregador

--- SistemaDeDelivery/src/main/java/models/usuarios/Entregador.java
+++ SistemaDeDelivery/src/main/java/models/usuarios/Entregador.java
@@ -3,26 +3,58 @@ package main.java.models.usuarios;
 import main.java.models.abstratos.Pessoa;
 import main.java.utils.InputHelper;
 
+/**
+ * Classe que representa um entregador do sistema.
+ * Um entregador é uma Pessoa que possui uma placa de veículo cadastrada.
+ */
 public class Entregador extends Pessoa {
 
-    private String palcaVeiculo;
+    private String placaVeiculo;
 
-    public Entregador(String nome, String cpf, String telefone,String email, String palcaVeiculo){
+    /**
+     * Construtor da classe Entregador.
+     *
+     * @param nome Nome completo do entregador
+     * @param cpf CPF do entregador
+     * @param telefone Telefone do entregador
+     * @param email Email do entregador
+     * @param placaVeiculo Placa do veículo utilizado pelo entregador
+     */
+    public Entregador(String nome, String cpf, String telefone, String email, String placaVeiculo) {
         super(nome, cpf, telefone, email);
-        this.palcaVeiculo = palcaVeiculo;
+        setPlacaVeiculo(placaVeiculo);
     }
 
     public String getPlacaVeiculo() {
         return placaVeiculo;
     }
-    public void setPalcaVeiculo(String palcaVeiculo) {
-        this.palcaVeiculo = palcaVeiculo;
+
+    /**
+     * Define a placa do veículo, aplicando validação simples.
+     */
+    public void setPlacaVeiculo(String placaVeiculo) {
+        if (placaVeiculo == null || placaVeiculo.trim().isEmpty()) {
+            throw new IllegalArgumentException("A placa do veículo não pode ser vazia!");
+        }
+
+        this.placaVeiculo = placaVeiculo.toUpperCase().trim();
     }
 
     @Override
     public void exibirDados() {
-        System.out.println("===ENTREGADOR===");
+        System.out.println("=== ENTREGADOR ===");
         InputHelper.exibirDadosPessoas(this);
-        System.out.println("Placa do veiculo : "+getPalcaVeiculo());
+        System.out.println("Placa do Veículo: " + placaVeiculo);
+    }
+
+    @Override
+    public String toString() {
+        return "Entregador {" +
+                "nome='" + getNome() + '\'' +
+                ", cpf='" + getCpf() + '\'' +
+                ", telefone='" + getTelefone() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", placaVeiculo='" + placaVeiculo + '\'' +
+                '}';
     }
 }
