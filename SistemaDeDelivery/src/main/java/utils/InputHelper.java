09c9792c7dc54273caafaa63d8230b160fb3934a
marcelocobra <marcelocobra54321@gmail.com>
2025-11-27 21:13:39 -0300

    Complementaçao da logica Main, adicionando as opçoes de escolha para executar o programa, e complementaçao da funçao inputHelper.

--- SistemaDeDelivery/src/main/java/utils/InputHelper.java
+++ SistemaDeDelivery/src/main/java/utils/InputHelper.java
@@ -2,16 +2,47 @@ package main.java.utils;
 
 import main.java.models.abstratos.Pessoa;
 import main.java.models.abstratos.Produto;
+import java.util.Scanner;
 
 public class InputHelper {
-    public static void exibirDadosPessoas(Pessoa p){
-        System.out.println("Nome: "+p.getNome());
-        System.out.println("CPF: "+p.getCpf());
-        System.out.println("Telefone: "+p.getCpf());
-        System.out.println("Email: "+p.getEmail());
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String lerString(String mensagem) {
+        System.out.print(mensagem);
+        return scanner.nextLine();
     }
-    public static void exbirDadosProduto(Produto p){
-        System.out.println("Nome: "+p.getNome());
-        System.out.println("Preço: "+p.getPreco());
+
+    public static int lerInt(String mensagem) {
+        while (true) {
+            try {
+                System.out.print(mensagem);
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, digite um número inteiro válido!");
+            }
+        }
+    }
+
+    public static double lerDouble(String mensagem) {
+        while (true) {
+            try {
+                System.out.print(mensagem);
+                return Double.parseDouble(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, digite um número decimal válido!");
+            }
+        }
+    }
+
+    public static void exibirDadosPessoas(Pessoa p) {
+        System.out.println("Nome: " + p.getNome());
+        System.out.println("CPF: " + p.getCpf());
+        System.out.println("Telefone: " + p.getTelefone());
+        System.out.println("Email: " + p.getEmail());
+    }
+
+    public static void exbirDadosProduto(Produto p) {
+        System.out.println("Nome: " + p.getNome());
+        System.out.println("Preço: R$ " + String.format("%.2f", p.getPreco()));
     }
 }
